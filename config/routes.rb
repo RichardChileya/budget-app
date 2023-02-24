7bd324e4810276f2f4113b274630abd9895a9f1c
RichardChileya <richardchileyawk@gmail.com>
2023-02-24 22:55:51 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   root "categories#index"
-    resources :users do
-    resources :categories do
-      resources :dealings
-    end
+  resources :categories, only:[ :index, :new, :show, :create] do
+    resources :dealings, only:[ :new, :create, :destroy]
   end
 end
+
+
+# root GET    /                                                                                                 categories#index
+# category_dealings POST   /categories/:category_id/dealings(.:format)                                                       dealings#create
+# new_category_dealing GET    /categories/:category_id/dealings/new(.:format)                                                   dealings#new
+#  category_dealing DELETE /categories/:category_id/dealings/:id(.:format)                                                   dealings#destroy
+#        categories GET    /categories(.:format)                                                                             categories#index
+#                   POST   /categories(.:format)                                                                             categories#create
+#      new_category GET    /categories/new(.:format)                                                                         categories#new
+#          category GET    /categories/:id(.:format)                                                                         categories#show
